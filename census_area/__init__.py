9323c7523ffa4415ae5f7f59e51b9ba21d44d7bf
Forest Gregg <fgregg@uchicago.edu>
2016-10-14 12:44:22 -0500

    factored out area filters

--- census_area/__init__.py
+++ census_area/__init__.py
@@ -11,81 +11,97 @@ BLOCK_2010_URL = 'https://tigerweb.geo.census.gov/arcgis/rest/services/TIGERweb/
 
 INCORPORATED_PLACES_TIGER = 'https://tigerweb.geo.census.gov/arcgis/rest/services/TIGERweb/tigerWMS_Current/MapServer/28'
 
+TRACT_URL = 'https://tigerweb.geo.census.gov/arcgis/rest/services/TIGERweb/tigerWMS_Current/MapServer/8'
+
 
 class Census(Census):
-    def geo_blockgroup(self, fields, geojson_geometry):
-        geo = shapely.geometry.shape(geojson_geometry)
-        geo_query_args = {'geometry': ',|'.join(str(x) for x in geo.bounds),
-                          'geometryType': 'esriGeometryEnvelope',
-                          'spatialRel': 'esriSpatialRelEnvelopeIntersects',
-                          'inSR' : '4326',
-                          'orderByFields': 'OID'}
-        bg_dumper = esridump.EsriDumper(BLOCK_GROUP_URL,
-                                        extra_query_args = geo_query_args)
+    def geo_tract(self, fields, geojson_geometry, return_geometry=False):
+        filtered_tracts = AreaFilter(geojson_geometry, TRACT_URL)
 
-        for block_group in bg_dumper:
-            block_group_geojson = block_group['geometry']
-            block_group_geo = shapely.geometry.shape(block_group_geojson)
-            if geo.intersects(block_group_geo):
-                context = {'state' : row['properties']['STATE'],
-                           'county' : row['properties']['COUNTY'],
-                           'tract' : row['properties']['TRACT']}
-                within = 'state:{state} county:{county} tract:{tract}'.format(**context)
-
-                block_group = row['properties']['BLKGRP']
-                (result,) = self.acs5.get(fields,
-                                          {'for': 'block group:{}'.format(block_group),
-                                           'in' :  within})
-
-                yield result
-
-    def geo_block(self, fields, geojson_geometry, return_geometry=False):
-        geo = shapely.geometry.shape(geojson_geometry)
-        geo_query_args = {'geometry': ',|'.join(str(x) for x in geo.bounds),
-                          'geometryType': 'esriGeometryEnvelope',
-                          'spatialRel': 'esriSpatialRelEnvelopeIntersects',
-                          'inSR' : '4326',
-                          'orderByFields': 'OID'}
-        block_dumper = esridump.EsriDumper(BLOCK_GROUP_URL,
-                                           extra_query_args = geo_query_args)
         features = []
-        num_blocks = 0
-        for block in block_dumper:
-            block_geojson = block['geometry']
-            block_geo = shapely.geometry.shape(block_geojson)
-            base_name = int(block['properties']['BASENAME'])
-            if base_name and geo.intersects(block_geo):
-                context = {'state' : block['properties']['STATE'],
-                           'county' : block['properties']['COUNTY'],
-                           'tract' : block['properties']['TRACT']}
-                within = 'state:{state} county:{county} tract:{tract}'.format(**context)
+        for tract in filtered_tracts:
+            context = {'state' : tract['properties']['STATE'],
+                       'county' : tract['properties']['COUNTY']}
+            within = 'state:{state} county:{county}'.format(**context)
 
-                block_id = '{}{:03d}'.format(block['properties']['BLKGRP'], base_name)
-                result = self.sf1.get(fields,
-                                      {'for': 'block:{}'.format(block_id),
-                                       'in' :  within})
-                if result:
-                    result, = result
-                    num_blocks += 1
-                    if num_blocks % 100 == 0:
-                        print('{} Blocks'.format(num_blocks), file=sys.stderr)
-                    if return_geometry:
-                        block['properties'].update(result)
-                        features.append(block)
-                    else:
-                        features.append(block)
+            tract_id = tract['properties']['TRACT']
+            result = self.acs5.get(fields,
+                                   {'for': 'tract:{}'.format(tract_id),
+                                    'in' :  within})
+
+            if result:
+                result, = result
+                if return_geometry:
+                    tract['properties'].update(result)
+                    features.append(tract)
+                else:
+                    features.append(result)
 
         if return_geometry:
             return {'type': "FeatureCollection", 'features': features}
         else:
             return features
-                
+    
+    def geo_blockgroup(self, fields, geojson_geometry):
+        filtered_block_groups = AreaFilter(geojson_geometry, BLOCK_GROUP_URL)
+
+        for block_group in filtered_block_groups:
+            context = {'state' : row['properties']['STATE'],
+                       'county' : row['properties']['COUNTY'],
+                       'tract' : row['properties']['TRACT']}
+            within = 'state:{state} county:{county} tract:{tract}'.format(**context)
+
+            block_group = row['properties']['BLKGRP']
+            result = self.acs5.get(fields,
+                                   {'for': 'block group:{}'.format(block_group),
+                                    'in' :  within})
+
+            if result:
+                result, = result
+                if return_geometry:
+                    tract['properties'].update(result)
+                    features.append(tract)
+                else:
+                    features.append(result)
+
+        if return_geometry:
+            return {'type': "FeatureCollection", 'features': features}
+        else:
+            return features
+
+    def geo_block(self, fields, geojson_geometry, return_geometry=False):
+        filtered_blocks = AreaFilter(geojson_geometry, BLOCK_2010_URL)
+
+        features = []
+        for block in filtered_blocks:
+            context = {'state' : block['properties']['STATE'],
+                       'county' : block['properties']['COUNTY'],
+                       'tract' : block['properties']['TRACT']}
+            within = 'state:{state} county:{county} tract:{tract}'.format(**context)
+
+            block_id = '{}{:03d}'.format(block['properties']['BLKGRP'], base_name)
+            result = self.sf1.get(fields,
+                                  {'for': 'block:{}'.format(block_id),
+                                   'in' :  within})
+            if result:
+                result, = result
+                if return_geometry:
+                    block['properties'].update(result)
+                    features.append(block)
+                else:
+                    features.append(result)
+
+        if return_geometry:
+            return {'type': "FeatureCollection", 'features': features}
+        else:
+            return features
+
 
     def state_place_blockgroup(self, fields, state, place):
         place_dumper = esridump.EsriDumper(INCORPORATED_PLACES_TIGER,
                                            extra_query_args = {'where' : "'{}'".format(place)})
         place = next(place_dumper)
-        place_geojson = esridump.esri2geojson(place['geometry']['coordinates'])
+        place_geojson = place['geometry']
 
         yield from self.geo_blockgroup(self, fields, place_geojson)
         
@@ -96,3 +112,23 @@ class Census(Census):
         place_geojson = place['geometry']
 
         return self.geo_block(fields, place_geojson, return_geometry)
+
+
+class AreaFilter(object):
+    def __init__(self, geojson_geometry, sub_geography_url):
+        self.geo = shapely.geometry.shape(geojson_geometry)
+
+        geo_query_args = {'geometry': ',|'.join(str(x) for x in self.geo.bounds),
+                          'geometryType': 'esriGeometryEnvelope',
+                          'spatialRel': 'esriSpatialRelEnvelopeIntersects',
+                          'inSR' : '4326',
+                          'orderByFields': 'OID'}
+        self.area_dumper = esridump.EsriDumper(sub_geography_url,
+                                               extra_query_args = geo_query_args)
+
+    def __iter__(self):
+        for area in self.area_dumper:
+            area_geo = shapely.geometry.shape(area['geometry'])
+            if self.geo.intersects(area_geo):
+                if self.geo.intersection(area_geo).area/area_geo.area > 0.1:
+                    yield area
